fde2d69c8fe3655c40248bce064cdf54e873c556
Alan Lin <lin_alan@network.lilly.com>
2022-10-30 20:19:19 -0700

    created a small python script that will keep my computer active

---
+++ stay_active/stay_active.py
@@ -0,0 +1,10 @@
+import pyautogui, time, random
+
+first = True
+while True:
+    if first:
+        time.sleep(10)
+        first = False
+    else:
+        time.sleep(random.randint(3, 15))
+    pyautogui.press("q")
---
+++ stay_active/stay_active.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /Users/L035653/Documents/Learning/web_development/ve/bin/activate
+python3 /Users/L035653/Documents/Learning/web_development/stay_active/stay_active.py
